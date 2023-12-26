4bfdd8e5a18e45b3171328816955c5e3708f7e3e
TinTinWinata <86914748+TinTinWinata@users.noreply.github.com>
2023-12-26 21:56:22 +0700

    feat: initiate frontend (emotion)

---
+++ backend/main.py
@@ -0,0 +1,37 @@
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+# Get all assistants
+# https://bluejack.binus.ac.id/lapi/api/Assistant/All
+
+# Picture
+# https://socs1.binus.ac.id/messier/GeneralWeb.svc/GetThumbnail/{picture_id}/300
+
+@app.route('/assistants')
+def assistants():
+  response = requests.get('https://bluejack.binus.ac.id/lapi/api/Assistant/All')
+  if response.status_code == 200:
+    data = response.json() # Return a dictionary
+    list = data.get('active', []) + data.get('inactive', [])
+    for value in list:
+      picture_id = value.get('PictureId', '')
+      value['PictureLink'] = f'https://socs1.binus.ac.id/messier/GeneralWeb.svc/GetThumbnail/{picture_id}/300'
+    return list
+  return 'Error occured', 500
+
+@app.route('/')
+def test():
+  
+  # Trigger the debugger activation
+  # x = 1 / 0
+
+  return 'Hello World'
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+
---
+++ frontend/src/styled/Button.tsx
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled'
+
+export const PrimaryButton = styled.button`
+  padding: 32px;
+`
